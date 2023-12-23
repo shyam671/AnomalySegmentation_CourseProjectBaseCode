aa1078daa563a6aed9c4dfda742e325aef6cf539
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-12-23 15:47:19 +0100

    msp, maxlogit and maxentropy done

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -12,6 +12,7 @@ from argparse import ArgumentParser
 from ood_metrics import fpr_at_95_tpr, calc_metrics, plot_roc, plot_pr, plot_barcode
 from sklearn.metrics import roc_auc_score, roc_curve, auc, precision_recall_curve, average_precision_score
 import torch.nn.functional as F
+import torch.nn.functional as F
 
 seed = 42
 
@@ -30,19 +31,21 @@ torch.backends.cudnn.benchmark = True
 def main():
     parser = ArgumentParser()
     parser.add_argument(
-        "--input",
-        default="/home/shyam/Mask2Former/unk-eval/RoadObsticle21/images/*.webp",
-        nargs="+",
-        help="A list of space separated input images; " "or a single glob pattern such as 'directory/*.jpg'",
-    )
-    parser.add_argument('--loadDir', default="../trained_models/")
-    parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
-    parser.add_argument('--loadModel', default="erfnet.py")
-    parser.add_argument('--subset', default="val")  # can be val or train (must have labels)
-    parser.add_argument('--datadir', default="/home/shyam/ViT-Adapter/segmentation/data/cityscapes/")
+        '--input',
+        default='/home/shyam/Mask2Former/unk-eval/RoadObsticle21/images/*.webp',
+        nargs='+',
+        help='A list of space separated input images; '
+        'or a single glob pattern such as "directory/*.jpg"',
+    )  
+    parser.add_argument('--loadDir',default='../trained_models/')
+    parser.add_argument('--loadWeights', default='erfnet_pretrained.pth')
+    parser.add_argument('--loadModel', default='erfnet.py')
+    parser.add_argument('--subset', default='val')  #can be val or train (must have labels)
+    parser.add_argument('--datadir', default='/content/cityscapes')
     parser.add_argument('--num-workers', type=int, default=4)
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--cpu', action='store_true')
+    parser.add_argument('--method', type=str, default='msp')
     parser.add_argument('--temperature', type=int, default=1)
     args = parser.parse_args()
     anomaly_score_list = []
@@ -55,8 +58,8 @@ def main():
     modelpath = args.loadDir + args.loadModel
     weightspath = args.loadDir + args.loadWeights
 
-    print("Loading model: " + modelpath)
-    print("Loading weights: " + weightspath)
+    print ('Loading model: ' + modelpath)
+    print ('Loading weights: ' + weightspath)
 
     model = ERFNet(NUM_CLASSES)
 
@@ -67,17 +70,17 @@ def main():
         own_state = model.state_dict()
         for name, param in state_dict.items():
             if name not in own_state:
-                if name.startswith("module."):
-                    own_state[name.split("module.")[-1]].copy_(param)
+                if name.startswith('module.'):
+                    own_state[name.split('module.')[-1]].copy_(param)
                 else:
-                    print(name, " not loaded")
+                    print(name, ' not loaded')
                     continue
             else:
                 own_state[name].copy_(param)
         return model
 
     model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    print("Model and weights LOADED successfully")
+    print ('Model and weights LOADED successfully')
     model.eval()
 
     # get scale temperature
@@ -88,31 +91,34 @@ def main():
         images = torch.from_numpy(np.array(Image.open(path).convert('RGB'))).unsqueeze(0).float()
         images = images.permute(0, 3, 1, 2)
         with torch.no_grad():
-            result = model(images)
-        anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
-
-        # MSP with temperature scaling
-        anomaly_result = 1.0 - np.max(F.softmax(result.squeeze(0).data.cpu().numpy() / temperature), axis=0)
-
-        pathGT = path.replace("images", "labels_masks")
-        if "RoadObsticle21" in pathGT:
-            pathGT = pathGT.replace("webp", "png")
-        if "fs_static" in pathGT:
-            pathGT = pathGT.replace("jpg", "png")
-        if "RoadAnomaly" in pathGT:
-            pathGT = pathGT.replace("jpg", "png")
+            result = model(images).squeeze(0)
+        if args.method == 'msp':
+            # MSP with temperature scaling
+            anomaly_result = 1.0 - torch.max(F.softmax(result / args.temperature, dim=0), dim=0)
+        elif args.method == 'maxlogit':
+            anomaly_result = 1.0 - torch.max(result, dim=0)
+        elif args.method == 'maxentropy':
+            anomaly_result = - torch.sum(F.softmax(result, dim=0) * F.log_softmax(result, dim=0), dim=0)
+        anomaly_result = anomaly_result.data.cpu().numpy()
+        pathGT = path.replace('images', 'labels_masks')
+        if 'RoadObsticle21' in pathGT:
+           pathGT = pathGT.replace('webp', 'png')
+        if 'fs_static' in pathGT:
+           pathGT = pathGT.replace('jpg', 'png')                
+        if 'RoadAnomaly' in pathGT:
+           pathGT = pathGT.replace('jpg', 'png')  
 
         mask = Image.open(pathGT)
         ood_gts = np.array(mask)
 
-        if "RoadAnomaly" in pathGT:
+        if 'RoadAnomaly' in pathGT:
             ood_gts = np.where((ood_gts == 2), 1, ood_gts)
-        if "LostAndFound" in pathGT:
+        if 'LostAndFound' in pathGT:
             ood_gts = np.where((ood_gts == 0), 255, ood_gts)
             ood_gts = np.where((ood_gts == 1), 0, ood_gts)
             ood_gts = np.where((ood_gts > 1) & (ood_gts < 201), 1, ood_gts)
 
-        if "Streethazard" in pathGT:
+        if 'Streethazard' in pathGT:
             ood_gts = np.where((ood_gts == 14), 255, ood_gts)
             ood_gts = np.where((ood_gts < 20), 0, ood_gts)
             ood_gts = np.where((ood_gts == 255), 1, ood_gts)
@@ -122,10 +128,12 @@ def main():
         else:
             ood_gts_list.append(ood_gts)
             anomaly_score_list.append(anomaly_result)
+            ood_gts_list.append(ood_gts)
+            anomaly_score_list.append(anomaly_result)
         del result, anomaly_result, ood_gts, mask
         torch.cuda.empty_cache()
 
-    file.write("\n")
+    file.write('\n')
 
     ood_gts = np.array(ood_gts_list)
     anomaly_scores = np.array(anomaly_score_list)
